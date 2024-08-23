3594195f6ba44aab9c4d97aa252aa20a67aad000
George Shalamberidze <giorgishalamberidze1995@gmail.com>
2024-08-23 12:28:59 +0400

    project initial setup + folder structure

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,17 @@
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="welcome" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+    </Stack>
+  );
+};
+
+export default Layout;
